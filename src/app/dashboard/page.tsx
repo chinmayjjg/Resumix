8f14d7e6ae09bfb7904bffe97937ac61bf143a0d
chinmay pradhan <147341405+chinmayjjg@users.noreply.github.com>
2025-12-03 17:10:50 +0530

    fixed some redirect issue

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -9,7 +9,7 @@ export default async function DashboardPage() {
   const session = await getServerSession(authOptions);
 
   if (!session?.user?.id) {
-    redirect('/auth/login');
+    redirect('/login');
   }
 
   await connectDB();
